12d3d133bec7a8a22ca5893dd05876a53bed0363
Eltrone <amineelorche@gmail.com>
2024-02-25 13:28:10 -0800

    file added

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,10 +1,11 @@
 for num in range(1, 51):  # Boucle de 1 à 50 inclus
-    if num % 3 == 0 and num % 5 == 0:  # Vérifie d'abord si divisible par 3 et 5
+    if num % 3 == 0 and num % 5 == 0:  # Vérif divisible par 3 et 5
         print("FizzBuzz", end=' ')
     elif num % 3 == 0:  # Ensuite, vérifie si divisible par 3
         print("Fizz", end=' ')
     elif num % 5 == 0:  # Ensuite, vérifie si divisible par 5
         print("Buzz", end=' ')
     else:
-        print(num, end=' ')  # Imprime le nombre s'il ne correspond à aucune des conditions ci-dessus
+        print(num, end=' ')
+        # Imprime nombre si ne correspond à aucune condition dessus
 print()
