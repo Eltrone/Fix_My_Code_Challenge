ba0277aa26eb8050872fdecdd887109caaaa9e9b
Eltrone <amineelorche@gmail.com>
2024-02-25 14:44:47 -0800

    file added

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -12,50 +12,49 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    dlistint_t *temp = NULL;
-    unsigned int i = 0;
+	dlistint_t *current = *head;
+	dlistint_t *temp = NULL;
+	unsigned int i = 0;
 
-    if (*head == NULL) /* Liste vide */
-    {
-        return (-1);
-    }
+	if (*head == NULL) /* Liste vide */
+	{
+		return (-1);
+	}
 
-    if (index == 0) /* Suppression du premier nœud */
-    {
-        temp = *head;
-        *head = (*head)->next;
-        if (*head != NULL) /* Si la liste contient plus d'un nœud */
-        {
-            (*head)->prev = NULL;
-        }
-        free(temp);
-        return (1);
-    }
+	if (index == 0) /* Suppression du premier nœud */
+	{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL) /* Si la liste contient plus d'un nœud */
+		{
+			(*head)->prev = NULL;
+		}
+		free(temp);
+		return (1);
+	}
 
-    while (i < index && current != NULL) /* Parcours jusqu'à l'index */
-    {
-        current = current->next;
-        i++;
-    }
+	while (i < index && current != NULL) /* Parcours jusqu'à l'index */
+	{
+		current = current->next;
+		i++;
+	}
 
-    if (current == NULL) /* Index hors de la portée */
-    {
-        return (-1);
-    }
+	if (current == NULL) /* Index hors de la portée */
+	{
+		return (-1);
+	}
 
-    if (current->next != NULL) /* Si le nœud à supprimer n'est pas le dernier */
-    {
-        current->next->prev = current->prev;
-    }
+	if (current->next != NULL) /* Si le nœud à supprimer n'est pas le dernier */
+	{
+		current->next->prev = current->prev;
+	}
 
-    if (current->prev != NULL) /* Si le nœud à supprimer n'est pas le premier */
-    {
-        current->prev->next = current->next;
-    }
+	if (current->prev != NULL) /* Si le nœud à supprimer n'est pas le premier */
+	{
+		current->prev->next = current->next;
+	}
 
-    free(current); /* Libère la mémoire du nœud à supprimer */
+	free(current); /* Libère la mémoire du nœud à supprimer */
 
-    return (1);
+	return (1);
 }
-
