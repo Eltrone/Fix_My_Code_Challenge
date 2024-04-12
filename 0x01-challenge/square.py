8df51d6b0b68238bdd8b3f6136aa2a4c706c2e08
Eltrone <amineelorche@gmail.com>
2024-04-12 12:27:06 +0000

    file added

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
+""" Defines a Square class. """
 
 
 class Square:
-    """
-    This class represents a square where width is equal to height.
-    """
-    size = 0
+    """ Represents a geometric square with width and height attributes. """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initializes a new Square instance with given dimensions. """
         for key, value in kwargs.items():
-            if key in {"size", "width", "height"}:
-                self.size = value
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """
-        Calculate the area of the square.
-        """
-        return self.size * self.size
+        """ Computes and returns the area of the square. """
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """
-        Calculate the perimeter of the square.
-        """
-        return 4 * self.size
+    def permiter_of_my_square(self):
+        """ Calculates and returns the perimeter of the square. """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.size, self.size)
+        """ Returns the string representation of the square dimensions. """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(size=12)
+    # Execution block to test Square functionality:
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
