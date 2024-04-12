218f070c48cc9d5fcc6791266fc54b98af6dfdfd
Eltrone <amineelorche@gmail.com>
2024-04-12 05:03:26 -0700

    file added

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,17 @@
-#!/usr/bin/python3
-
-class Square:
-    """
-    This class represents a square where width is equal to height,
-    with a single attribute for size.
-    """
-    size  0
-
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            if key in {"size", "width", "height"}:
-                self.size = value
+ass Square:
+    def __init__(self, size=0):
+        self.size = size
 
     def area_of_my_square(self):
-        """
-        Calculate the area of the square using the formula size * size.
-        """
+        """ Area of the square """
         return self.size * self.size
 
     def perimeter_of_my_square(self):
-        """
-        Calculate the perimeter of the square using the formula 4 * size.
-        """
+        """ Perimeter of the square """
         return 4 * self.size
 
     def __str__(self):
         return "{}/{}".format(self.size, self.size)
-
 
 if __name__ == "__main__":
     s = Square(size=12)
